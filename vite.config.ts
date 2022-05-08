79da7df74a9433de4d1fe9fa23b5aab007deb633
dbnryanc92 <dbnryanc92@gmail.com>
2022-05-08 11:41:15 +0800

    Update dev config

--- vite.config.ts
+++ vite.config.ts
@@ -58,6 +58,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  /*
   server: {
     open: false,
     port: 443,
@@ -66,6 +67,7 @@ export default defineConfig({
       cert: fs.readFileSync("./certs/server.crt"),
     },
   },
+  */
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
